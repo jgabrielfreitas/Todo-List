bedb6ac50085fd526f4187b0bd139ca607728c70
JGabrielFreitas <jgabrielafreitas@gmail.com>
2016-10-19 02:25:15 -0200

    adding Facebook Stetho

--- core/src/main/java/com/jgabrielfreitas/core/application/CoreApplication.kt
+++ core/src/main/java/com/jgabrielfreitas/core/application/CoreApplication.kt
@@ -2,6 +2,7 @@ package com.jgabrielfreitas.core.application
 
 import android.app.Application
 import com.activeandroid.ActiveAndroid
+import com.facebook.stetho.Stetho
 
 /**
  * Created by JGabrielFreitas on 03/10/16.
@@ -11,6 +12,7 @@ class CoreApplication : Application() {
     override fun onCreate() {
         super.onCreate()
 
+        Stetho.initializeWithDefaults(this)
         ActiveAndroid.initialize(this)
     }
 }
