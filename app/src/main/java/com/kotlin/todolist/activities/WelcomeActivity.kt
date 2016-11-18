8b142a7f2f3bfe3af526912a795af71e807783f3
JGabrielFreitas <jgabrielafreitas@gmail.com>
2016-11-17 23:13:33 -0200

    updating to appcompat-v7:25.0.0

--- app/src/main/java/com/kotlin/todolist/activities/WelcomeActivity.kt
+++ app/src/main/java/com/kotlin/todolist/activities/WelcomeActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.support.v4.app.Fragment
 import com.github.paolorotolo.appintro.AppIntro
 import com.kotlin.todolist.R
+import com.kotlin.todolist.R.color.*
+import com.kotlin.todolist.R.drawable.*
 import com.kotlin.todolist.R.string.*
 import com.kotlin.todolist.fragments.SlideFragment
 
@@ -13,13 +15,9 @@ class WelcomeActivity : AppIntro() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-//        addSlide(newInstance(getString(title_sync), getString(msg_now_you_can_sync_tasks), cloud, R.color.cloud_background))
-//        addSlide(newInstance(getString(title_check), getString(msg_easy_tasks), check, R.color.cloud_background))
-//        addSlide(newInstance(getString(title_focus), getString(msg_keep_in_focus), man, R.color.cloud_background))
-//        addSlide(newInstance(getString(title_focus), getString(msg_keep_in_focus), man, R.color.cloud_background))
-        addSlide(SlideFragment(getString(title_sync)))
-        addSlide(SlideFragment(getString(title_check)))
-        addSlide(SlideFragment(getString(title_focus)))
+        addSlide(SlideFragment(getString(title_sync),  getString(msg_now_you_can_sync_tasks), cloud, cloud_background))
+        addSlide(SlideFragment(getString(title_check), getString(msg_easy_tasks),             check, check_background))
+        addSlide(SlideFragment(getString(title_focus), getString(msg_keep_in_focus),          man,   person_background))
 
         setSkipText(getString(R.string.skip))
         setDoneText(getString(R.string.done))
@@ -32,5 +30,6 @@ class WelcomeActivity : AppIntro() {
     private fun startApplication() {
         val startAppIntent = Intent(this, MainActivity::class.java)
         startActivity(startAppIntent)
+        finish()
     }
 }
--- app/src/main/java/com/kotlin/todolist/fragments/SlideFragment.kt
+++ app/src/main/java/com/kotlin/todolist/fragments/SlideFragment.kt
@@ -1,5 +1,6 @@
 package com.kotlin.todolist.fragments
 
+import android.support.v4.content.ContextCompat
 import com.jgabrielfreitas.layoutid.annotations.InjectLayout
 import com.jgabrielfreitas.layoutid.fragments.InjectLayoutBaseFragment
 import com.kotlin.todolist.R
@@ -9,13 +10,26 @@ import kotlinx.android.synthetic.main.fragment_slide.*
  * Created by JGabrielFreitas on 06/11/16.
  */
 @InjectLayout(layout = R.layout.fragment_slide)
-class SlideFragment : InjectLayoutBaseFragment {
+class SlideFragment(title: String, content: String, drawable: Int, color: Int) : InjectLayoutBaseFragment() {
 
-    constructor() {
+    var title    : String = ""
+    var content  : String = ""
+    var drawable : Int    = 0
+    var color    : Int    = 0
+
+    init {
+        this.title    = title
+        this.content  = content
+        this.drawable = drawable
+        this.color    = color
     }
 
-    constructor(title: String) : super() {
-
-        titleTextView.text = title
+    override fun onStart() {
+        super.onStart()
+        titleTextView.text   = title
+        contentTextView.text = content
+        introImageView.setImageDrawable(ContextCompat.getDrawable(activity, drawable))
+        rootLinearLayout.setBackgroundColor(ContextCompat.getColor(activity, color))
     }
+
 }
